87d79900399e3993695d47c0d7ac4140d69b430b
Jay Lee <jaylee.possible@gmail.com>
2025-04-13 21:52:02 +0900

    feat: initialize react project

--- components/widgets/ButtonWidget/index.tsx
+++
@@ -1,5 +0,0 @@
-import { ButtonWidgetProps } from '../../../types';
-
-export default function ButtonWidget({ title, onClick }: ButtonWidgetProps) {
-  return <button onClick={onClick}>{title}</button>;
-}
